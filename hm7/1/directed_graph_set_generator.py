e0df23707288c64f482d06fbf716f9616431b61d
Dalton Cole <dalton_cl@yahoo.com>
2017-02-01 18:46:32 -0600

    Removed 7

--- hm7/1/directed_graph_set_generator.py
+++
@@ -1,9 +0,0 @@
-import os
-import subprocess
-import time
-
-for i in range(1000):
-	proc = subprocess.Popen(["./random_graph/directed.out " + str(i)], stdout=subprocess.PIPE, shell=True)
-	out = proc.communicate()[0].decode('ascii')
-	with open('directed_graphs/' + str(i), 'w') as f:
-		f.write(out)
--- hm7/3/TRANSITIVE-CLOSURE/REPORT.md
+++
@@ -1,3 +0,0 @@
-The time complexity of the program is O(V^3). This can be seen by the triple for loop with a range from 0 to n-1. 
-
-The output with the transitive closure algorithm ran on the random graphs from the Floyd-Warshall algorithm can be seen in the random_graph folder.
